95c98a4e33538b1813adc5f0456e8ce816185b25
JamahlBoykin <118788306+JamahlBoykin@users.noreply.github.com>
2022-12-24 17:17:36 -0800

    Change background color of tile upon mouse enter

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ for (let i = 0; i < (16*16); i++) {
   tile.classList.add('tile');
   grid.appendChild(tile);
 }
+
+const tiles = document.querySelectorAll('.tile');
+tiles.forEach((tile) => {
+  tile.addEventListener('mouseenter', () => {
+    tile.style.backgroundColor = 'aqua';
+  });
+});
