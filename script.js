6e660da30641b1b2adde95a3b46d71525addbe1b
JamahlBoykin <118788306+JamahlBoykin@users.noreply.github.com>
2022-12-24 19:29:14 -0800

    Make grid size and tile count responsive

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 const grid = document.querySelector('.grid');
+createGrid();
 
-for (let i = 0; i < (16*16); i++) {
-  const tile = document.createElement('div');
-  tile.classList.add('tile');
-  grid.appendChild(tile);
+function createGrid(gridSize = 16) {
+  resetGrid();
+  setDimensions(gridSize);
+  createTiles(gridSize ** 2);
+  addListeners();
 }
 
-const tiles = document.querySelectorAll('.tile');
-tiles.forEach((tile) => {
-  tile.addEventListener('mouseenter', () => {
-    tile.style.backgroundColor = 'aqua';
+function resetGrid() {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+}
+
+function setDimensions(size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+}
+
+function createTiles(tileCount) {
+  for (let i = 0; i < (tileCount); i++) {
+    const tile = document.createElement('div');
+    tile.classList.add('tile');
+    grid.appendChild(tile);
+  }
+}
+
+function addListeners() {
+  const tiles = document.querySelectorAll('.tile');
+  tiles.forEach((tile) => {
+    tile.addEventListener('mouseenter', () => {
+      tile.style.backgroundColor = 'aqua';
+    });
   });
-});
+}
